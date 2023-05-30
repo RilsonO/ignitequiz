10a4e722632bc4f5f0e4144b4d87a1caf21564ec
Rilson Oliveira <rilson2.0@gmail.com>
2023-05-30 14:52:31 -0300

    feat: Add animation in QuizCard component.

--- src/components/QuizCard/index.tsx
+++ src/components/QuizCard/index.tsx
@@ -1,4 +1,13 @@
-import { TouchableOpacity, TouchableOpacityProps, Text, View } from 'react-native';
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Text,
+  View,
+} from 'react-native';
+import Animated, { FadeInUp } from 'react-native-reanimated';
+
+const TouchableOpacityAnimated =
+  Animated.createAnimatedComponent(TouchableOpacity);
 
 import { styles } from './styles';
 import { THEME } from '../../styles/theme';
@@ -7,14 +16,16 @@ import { LevelBars } from '../LevelBars';
 import { QUIZZES } from '../../data/quizzes';
 
 type Props = TouchableOpacityProps & {
-  data: typeof QUIZZES[0];
-}
+  data: (typeof QUIZZES)[0];
+  index: number;
+};
 
-export function QuizCard({ data, ...rest }: Props) {
+export function QuizCard({ data, index, ...rest }: Props) {
   const Icon = data.svg;
 
   return (
-    <TouchableOpacity
+    <TouchableOpacityAnimated
+      entering={FadeInUp.delay(index * 100)}
       style={styles.container}
       {...rest}
     >
@@ -26,9 +37,7 @@ export function QuizCard({ data, ...rest }: Props) {
         <LevelBars level={data.level} />
       </View>
 
-      <Text style={styles.title}>
-        {data.title}
-      </Text>
-    </TouchableOpacity>
+      <Text style={styles.title}>{data.title}</Text>
+    </TouchableOpacityAnimated>
   );
 }
